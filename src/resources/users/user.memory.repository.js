be17c4e8e88c7619fa51091d91e2834278abae8f
Katsiaryna Laurynovich <katsiaryna_laurynovich@epam.com>
2020-04-04 18:43:48 +0200

    Express REST Service:
    initial routing for users

--- src/resources/users/user.memory.repository.js
+++ src/resources/users/user.memory.repository.js
@@ -1,6 +1,38 @@
+const data = [
+  {
+    id: '1',
+    name: 'name',
+    login: 'login',
+    password: 'password'
+  },
+  {
+    id: '2',
+    name: 'name2',
+    login: 'login2',
+    password: 'password'
+  }
+];
+
 const getAll = async () => {
-  // TODO: mock implementation. should be replaced during task development
-  return [];
+  return data;
 };
 
-module.exports = { getAll };
+const createUser = async user => {
+  data.push(user);
+
+  return user;
+};
+
+const updateUser = async (id, user) => {
+  const index = data.findIndex(i => i.id === id);
+  data[index] = { id, ...user };
+
+  return data[index];
+};
+
+const deleteUser = id => {
+  const i = data.findIndex(user => user.id === id);
+  return data.splice(i, 1);
+};
+
+module.exports = { getAll, createUser, updateUser, deleteUser };
--- src/resources/users/user.router.js
+++ src/resources/users/user.router.js
@@ -2,10 +2,51 @@ const router = require('express').Router();
 const User = require('./user.model');
 const usersService = require('./user.service');
 
-router.route('/').get(async (req, res) => {
-  const users = await usersService.getAll();
-  // map user fields to exclude secret fields like "password"
-  res.json(users.map(User.toResponse));
-});
+router
+  .route('/')
+  .get(async (req, res) => {
+    const users = await usersService.getAll();
+
+    res.json(users.map(User.toResponse));
+  })
+  .post(async (req, res) => {
+    const data = await usersService.createUser(new User({ ...req.body }));
+
+    if (data) {
+      res.status(200).json(User.toResponse(data));
+    } else {
+      res.status(404).json('Error');
+    }
+  });
+
+router
+  .route('/:id')
+  .get(async (req, res) => {
+    const user = await usersService.getUserById(req.params.id);
+
+    if (user.length === 0) {
+      res.status(404).json('User with given ID is not found.');
+    } else {
+      res.json(user.map(User.toResponse)[0]);
+    }
+  })
+  .put(async (req, res) => {
+    const result = await usersService.updateUser(req.params.id, req.body);
+
+    if (result) {
+      res.status(200).json(User.toResponse(result));
+    } else {
+      res.status(404).json('User with given ID is not found.');
+    }
+  })
+  .delete(async (req, res) => {
+    const result = await usersService.deleteUser(req.params.id);
+
+    if (result) {
+      res.status(200).json(result);
+    } else {
+      res.status(404).json('User with given ID is not found.');
+    }
+  });
 
 module.exports = router;
